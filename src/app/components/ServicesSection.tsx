101ed6463aabecb0948a8d5fbb06ad7bc5cedfee
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-02-02 15:37:11 +0600

    service details page done

--- src/app/components/ServicesSection.tsx
+++ src/app/components/ServicesSection.tsx
@@ -1,6 +1,7 @@
 import dbConnect from '@/lib/dbConnect';
-import Service from '@/models/Services';
+import Service from '@/models/Service';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 
 export async function fetchServices() {
@@ -13,14 +14,16 @@ export default async function ServicesSection() {
   const data = (await fetchServices()) || [];
 
   return (
-    <div className="grid grid-cols-12 container mx-auto gap-6">
+    <div className="grid grid-cols-12 container mx-auto gap-6 my-20">
       {data.map((service) => (
         <div key={service?._id} className="col-span-4 border shadow-sm p-4 rounded-md">
           <Image className="rounded-md" src={service.img} alt={service?.img} width={314} height={208} />
           <h2 className="my-2 text-2xl font-medium">{service?.title}</h2>
           <div className="flex items-center justify-between">
             <p className="text-xl text-orange-500 font-medium">Price: ${service?.price}</p>
-            <FaArrowRight />
+            <Link href={`/services/${service?._id}`}>
+              <FaArrowRight />
+            </Link>
           </div>
         </div>
       ))}
---
+++ src/app/services/[id]/page.tsx
@@ -0,0 +1,49 @@
+import dbConnect from '@/lib/dbConnect';
+import Service from '@/models/Service';
+import Image from 'next/image';
+
+interface ServiceDetailsProps {
+  params: { id: string };
+}
+
+interface serviceDocument {
+  _id: string;
+  service_id: string;
+  title: string;
+  img: string;
+  price: string;
+  description: string;
+  facility: {
+    name: string;
+    details: string;
+  }[];
+}
+
+export default async function ServiceDetails({ params }: ServiceDetailsProps) {
+  const { id } = params;
+
+  await dbConnect();
+  const data = ((await Service.findById(id)) as serviceDocument) || {};
+
+  return (
+    <div className="w-10/12 mx-auto">
+      <div>
+        <Image src={data?.img} width={500} height={400} alt={data?.title} />
+      </div>
+      <div className="mt-4 space-y-1">
+        <div className="flex justify-between">
+          <h1 className="text-3xl font-semibold">{data?.title}</h1>
+          <p className="text-orange-500 text-xl font-bold">${data?.price}</p>
+        </div>
+        <p>{data?.description}</p>
+
+        {data?.facility.map((facility, index) => (
+          <div key={index}>
+            <h2 className="font-bold text-lg">{facility?.name}</h2>
+            <p>{facility?.details}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
